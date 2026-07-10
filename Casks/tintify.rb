38ac74f20b0d9c7b999a1a9d43b437cf3fb90105
notwin <notwin@users.noreply.github.com>
2026-07-10 14:47:53 +0900

    tintify 1.9.3

--- Casks/tintify.rb
+++ Casks/tintify.rb
@@ -1,6 +1,6 @@
 cask "tintify" do
-  version "1.9.2"
-  sha256 "4194590186dac998608fad3846e0ec24cb19be1733372e887666af025e686dc6"
+  version "1.9.3"
+  sha256 "750e9b5c02704ab43541c73ed36539e34e327c44f6872713f0fac40984a18398"
 
   url "https://github.com/notwin/Tintify/releases/download/v#{version}/Tintify-#{version}.dmg"
   name "Tintify"
